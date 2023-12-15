62340fff39d697adfc80a1601b5265d39ac4199f
Ahliddin <ahlturanov@gmail.com>
2023-12-16 01:31:38 +0500

    Add Tests for all projects

---
+++ TestNinja/TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    internal class EmployeeControllerTests
+    {
+        EmployeeController _employeeController;
+        Mock<IEmployeeStorage> _employeeStorage;
+        [SetUp]
+        public void SetUp()
+        {
+            _employeeStorage = new Mock<IEmployeeStorage>();
+            _employeeController = new EmployeeController(_employeeStorage.Object);
+        }
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeletesEmployee()
+        {
+            int id = 0;
+
+            _employeeController.DeleteEmployee(id);
+
+            _employeeStorage.Verify(es => es.DeleteEmployeeStorage(id));
+            Assert.That(_employeeController.DeleteEmployee(id), Is.TypeOf<RedirectResult>());
+        }
+    }
+}
---
+++ TestNinja/TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using TestNinja.Mocking.InstallerHelper;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    internal class InstallerHelperTests
+    {
+        Mock<IInstallerGetter> _installerGetter;
+        InstallerHelper _installerHelper;
+        [SetUp]
+        public void SetUp()
+        {
+            _installerGetter = new Mock<IInstallerGetter>();
+            _installerHelper = new InstallerHelper();
+        }
+        [Test]
+        public void DownloadInstaller_WhenCalled_ReturnTrue()
+        {
+            _installerGetter.Setup(id => id.DownloadInstaller("a", "a")).Returns(true);
+
+            Assert.That(_installerHelper.DownloadInstallerHelper("a", "a", _installerGetter.Object), Is.True);
+        }
+        [Test]
+        public void DownloadInstaller_InvalidArguments_ReturnFalse()
+        {
+            _installerGetter.Setup(id => id.DownloadInstaller("", "")).Returns(false);
+
+            Assert.That(_installerHelper.DownloadInstallerHelper("", "", _installerGetter.Object), Is.False);
+        }
+    }
+}
